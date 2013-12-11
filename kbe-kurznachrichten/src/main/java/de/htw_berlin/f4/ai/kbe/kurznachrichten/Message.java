0f772a9ac123ec3a2af281c6b00163ccff69c38e
Denis Neuling <denisneuling@gmail.com>
2013-12-11 14:03:40 +0100

    Init

---
+++ kbe-kurznachrichten/src/main/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/Message.java
@@ -0,0 +1,68 @@
+package de.htw_berlin.f4.ai.kbe.kurznachrichten;
+
+import java.util.Date;
+
+public class Message {
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Boolean isOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(Boolean origin) {
+		this.origin = origin;
+	}
+
+	public Long getMessageId() {
+		return messageId;
+	}
+
+	public void setMessageId(Long messageId) {
+		this.messageId = messageId;
+	}
+
+	// creationDate
+	private Date date;
+
+	private String content;
+
+	private String topic;
+
+	private User user;
+
+	private Boolean origin;
+
+	private Long messageId;
+
+}
